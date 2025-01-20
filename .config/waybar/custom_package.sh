ecb96583bb4832861198609de0507e02115727f7
Ark1409 <ark1409dev@gmail.com>
2025-01-20 04:47:37 -0800

    Added package list in waybar package module

--- .config/waybar/custom_package.sh
+++ .config/waybar/custom_package.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-update_count=$(checkupdates --nocolor | wc -l)
+update_output=$(checkupdates --nocolor)
+update_count=$(wc -l <<< "$update_output")
+update_packages=$(sed 's/ .*//g' <<< "$update_output")
 
 if [[ $update_count == 0 ]]; then
     cat <<< '{"text": "", "alt": "", "tooltip": "", "class": "", "percentage": 0 }'
     exit 0
 fi
 
+tooltip=""
+
+if [[ $update_count == 1 ]]; then
+    tooltip="$update_count pending package"
+else
+    tooltip="$update_count pending packages"
+fi
+
+tooltip+="\n\n$update_packages"
+
 cat <<EOF
-{"text": "$update_count", "alt": "", "tooltip": "$update_count pending package(s)", "class": "", "percentage": 100 }
+{"text": "$update_count", "alt": "", "tooltip": "$tooltip", "class": "", "percentage": 100 }
 EOF
