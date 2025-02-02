0e146b6df7e372d89645990f718505e147a44ec0
Ark1409 <ark1409dev@gmail.com>
2025-02-02 01:32:46 -0800

    Put scripts into hypr config directory

---
+++ .config/hypr/scripts/hyprclose.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkfile() {
+    mkdir -p "$(dirname $1)" || return
+    [ -f "$1" ] || touch "$1"
+}
+
+mkfile /tmp/hypr/hyprclose.log
+
+# Close a few times for any confirmation messages
+for i in "$(seq 1 3)"; do
+    HYPRCMDS=$(hyprctl -j clients | jq -j '.[] | "dispatch closewindow address:\(.address); "')
+    hyprctl --batch "$HYPRCMDS" > /tmp/hypr/hyprclose.log 2>&1
+done
+
+systemctl --user stop hyprpolkitagent
+
+sleep 0.25 && hyprctl dispatch exit
---
+++ .config/hypr/scripts/idlectl.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+prog_name=$(basename "$0")
+
+print_help() {
+    cat <<EOF
+usage: $prog_name [level]
+Manages stages of screen idle process.
+
+    level: an integer in the range [0, 4], with 0 representing the default state.
+EOF
+}
+
+if [ "$#" != 1 ]; then
+    print_help
+    exit 1
+fi
+
+case "$1" in
+    0)
+        hyprctl dispatch dpms on
+        brightnessctl -r
+        ;;
+    1)
+        brightnessctl -s
+        brightnessctl s $(($(brightnessctl g)*3/4))
+        ;;
+    2)
+        brightnessctl s $(($(brightnessctl g)/3))
+        ;;
+    3)
+        hyprctl dispatch dpms off &
+        loginctl lock-session
+        ;;
+    4)
+        systemctl suspend
+        ;;
+    *)
+        print_help
+        exit 1
+        ;;
+esac
